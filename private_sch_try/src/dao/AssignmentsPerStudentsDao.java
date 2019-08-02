1472dc0ac0f753b172753c70be9300b13071a8cf
chrisdoulamis <47662349+chrisdoulamis@users.noreply.github.com>
2019-08-02 15:06:36 +0300

    Update AssignmentsPerStudentsDao.java

--- private_sch_try/src/dao/AssignmentsPerStudentsDao.java
+++ private_sch_try/src/dao/AssignmentsPerStudentsDao.java
@@ -15,7 +15,7 @@ public class AssignmentsPerStudentsDao {
 
     private final String URL = "jdbc:mysql://localhost:3306/loco?serverTimezone=UTC";
     private final String USERNAME = "root";
-    private final String PASS = "a2MERA3minka";
+    private final String PASS = "*********";
     private Connection conn;
 
     private Connection getConnection() {
