0ca43bbb27e4e1ee6648a55c834e02918f113c20
chrisdoulamis <47662349+chrisdoulamis@users.noreply.github.com>
2019-08-02 15:09:31 +0300

    Update TrainersPerCourseDao.java

--- private_sch_try/src/dao/TrainersPerCourseDao.java
+++ private_sch_try/src/dao/TrainersPerCourseDao.java
@@ -20,7 +20,7 @@ public class TrainersPerCourseDao {
 
     private final String URL = "jdbc:mysql://localhost:3306/privateschool?serverTimezone=UTC";
     private final String USERNAME = "root";
-    private final String PASS = "a2MERA3minka";
+    private final String PASS = "*********";
     private Connection conn;
 
     private Connection getConnection() {
